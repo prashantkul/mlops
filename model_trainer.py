8e3b7395f5ea81a9ab2acba67ee0150b44ad6a45
SVeldman <112725698+SVeldman@users.noreply.github.com>
2024-12-07 21:33:12 -0500

    added gcloud_handler.py

--- model_trainer.py
+++ model_trainer.py
@@ -4,12 +4,12 @@ from typing import List, Dict, Optional, Any
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import SMOTE
 from config_manager import ConfigManager
 
 import h2o
 from h2o.automl import H2OAutoML
+
+from gcloud_handler import GCSHandler
 
 from dvc_operations.download import DVCReader
 
@@ -17,51 +17,28 @@ class ModelTrainer:
     """Handles model training and evaluation for credit risk prediction"""
     
     def __init__(self, file_path = "cred_card_featured_engg_train.csv", to_split: bool = True):
-        self.features = ConfigManager().get_config('features')  # Feature column names from config
-        self.file_path = file_path
-        self.data = None
-        self.train = None
-        self.test = None
-        self.drift = None
-        self.to_split = to_split
-        #self.val = None
-        #self.X_test = None
-        #self.y_test = None
-        
-    def load_training_data(self, split = 0.1, resample = True):
-        """Prepare data for training"""
-        
+        config = ConfigManager()
         reader = DVCReader()
+        self.features = config.get_config('features')  # Feature column names from config
+        self.file_path = file_path
         self.data = reader.read_dataframe(self.file_path)
+        self.h2o_port = config.get_config('h2o_port')   # h2o port from config
+        self.h2o_ip = config.get_config('h2o_ip')   # h2o ip from config
+        
+    def train_model(self):
+        """Train Model Using H2O AutoML"""
 
+        # Verify correct columns:
         column_list = self.features
         column_list.append('target')
 
-        # Verify correct columns:
         if column_list != self.data.columns.tolist():
             drop_cols = [col for col in self.data.columns if col not in self.features + ['target']]
             self.data = self.data.drop(columns=drop_cols)
         
-        '''
-        # Synthetically generate extra data for minority class (this is only for the hypothetical of our project, not for real-world application/production):
-        if resample == True:
-            smote = SMOTE(sampling_strategy=0.25, random_state=10086)
-            X_resampled, y_resampled = smote.fit_resample(self.data.drop(columns=['target']), self.data['target'])
-            resampled = pd.DataFrame(X_resampled, columns=features)
-            resampled['target'] = y_resampled
-            self.data = resampled
-
-        if self.to_split == True:
-            self.train, self.test = train_test_split(self.data, test_size=split, random_state=10086)
-        else:
-            self.train = self.data
-        '''        
-
-    def train_model(self):
-        """Train Model Using H2O AutoML"""
         # Initialize H2O:
-        #h2o = h2o.init(port = h20_port, ip = h20_ip)
-        h2o = h2o.init(port = 54321, ip = "35.184.233.137")
+        #h2o = h2o.init(port = 54321, ip = "35.184.233.137")
+        h2o = h2o.init(port = self.h2o_port, ip = self.h2o_ip)
 
         # Convert training data to H2OFrame:
         h2o_data = h2o.H2OFrame(self.train)
@@ -78,5 +55,24 @@ class ModelTrainer:
 
         self.model = aml.leader
         h2o.save_model(self.model, path='models/credit_risk_model')
+
+        # Upload model to GCP:
+        gcs = GCSHandler()
+        gcs.handle_file("upload", "models/credit_risk_model", "models/credit_risk_model")
+
+        '''
+        # Synthetically generate extra data for minority class (this is only for the hypothetical of our project, not for real-world application/production):
+        if resample == True:
+            smote = SMOTE(sampling_strategy=0.25, random_state=10086)
+            X_resampled, y_resampled = smote.fit_resample(self.data.drop(columns=['target']), self.data['target'])
+            resampled = pd.DataFrame(X_resampled, columns=features)
+            resampled['target'] = y_resampled
+            self.data = resampled
+
+        if self.to_split == True:
+            self.train, self.test = train_test_split(self.data, test_size=split, random_state=10086)
+        else:
+            self.train = self.data
+        '''        
 
 
